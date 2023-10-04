c1ab7e7b3bbe54ebd753eb43898da001ccb55126
Roy <roy@roymckenzie.me>
2023-10-04 16:40:18 -0500

    - Check for clients that are not alive every 30 seconds and terminate - Set

--- src/server.js
+++ src/server.js
@@ -12,68 +12,88 @@ const server = express()
 
 const wss = new ws.Server({ server });
 
-wss.on('connection', (socket) => {
+wss
+  .on('connection', (socket) => {
 
-  /** @type {Set<string>} */
-  const subscribedTopics = new Set();
+    socket.on('error', console.error);
+    socket.isAlive = true;
 
-  socket.on('message', (data) => {
-    const message = JSON.parse(data.toString());
-    
-    switch (message.type) {
-      case 'subscribe':
-        message.topics.forEach(topicName => {
-          subscribedTopics.add(topicName);
+    /** @type {Set<string>} */
+    const subscribedTopics = new Set();
 
+    socket
+      .on('message', (data) => {
+        const message = JSON.parse(data.toString());
+        
+        switch (message.type) {
+
+          case 'subscribe':
+            message.topics.forEach(topicName => {
+              subscribedTopics.add(topicName);
+
+              const subscribers = topics.get(topicName) || new Set();
+              subscribers.add(socket);
+
+              if (!topics.has(topicName)) {
+                topics.set(topicName, subscribers);
+              }
+            });
+            break;
+
+          case 'publish':
+            const subscribers = topics.get(message.topic) || new Set();
+
+            message.clients = subscribers.size;
+
+            subscribers.forEach(subscriber => {
+              if (socket === subscriber) return; // Don't send publish messages to self
+              subscriber.send(JSON.stringify(message));
+            });
+            break;
+
+          case 'unsubscribe':
+            message.topics.forEach(topicName => {
+              const subscribers = topics.get(topicName);
+
+              if (subscribers) {
+                subscribers.delete(socket);
+              }
+            });
+            break;
+
+          case 'ping':
+            socket.send(JSON.stringify({ type: 'pong' }));
+            socket.isAlive = true;
+            break;
+
+          default:
+            console.log(`Unhandled message: ${message}`);
+            break;
+        }
+      })
+      .on('close', (code) => {
+        subscribedTopics.forEach(topicName => {
           const subscribers = topics.get(topicName) || new Set();
-          subscribers.add(socket);
-
-          if (!topics.has(topicName)) {
-            topics.set(topicName, subscribers);
+          subscribers.delete(socket);
+          if (subscribers.size === 0) {
+            topics.delete(topicName);
           }
         });
-        break;
-      case 'publish':
-        const subscribers = topics.get(message.topic) || new Set();
-        message.clients = subscribers.size;
-        subscribers.forEach(subscriber => {
-          subscriber.send(JSON.stringify(message));
-        });
-        break;
-      case 'unsubscribe':
-        message.topics.forEach(topicName => {
-          const subscribers = topics.get(topicName);
+        subscribedTopics.clear();
 
-          if (subscribers) {
-            subscribers.delete(socket);
-          }
-        });
-        break;
-      case 'ping':
-        socket.send(JSON.stringify({ type: 'pong' }));
-        break;
-      default:
-        break;
-    }
-  });
-
-  socket.on('close', (code) => {
-    subscribedTopics.forEach(topicName => {
-      const subscribers = topics.get(topicName) || new Set();
-      subscribers.delete(socket);
-      if (subscribers.size === 0) {
-        topics.delete(topicName);
-      }
-    });
-    subscribedTopics.clear();
-
-    if (code === 1005) {
-      socket.close();
-    }
-  });
-
-  socket.on('error', (error) => {
-    console.log(error);
+        if (code === 1005) {
+          socket.terminate();
+        }
+      });
   })
+  .on('close', () => {
+    clearInterval(checkAliveInterval);
+  });
 
-});
+const checkAliveInterval = setInterval(() => {
+  wss.clients.forEach(socket => {
+    if (socket.isAlive === false) return socket.terminate();
+
+    socket.isAlive = false;
+  });
+}, 30000);
